a6e1f0d39c887814d4119406b0aceffac67b0e38
weslley41 <41weslley@gmail.com>
2023-02-07 20:51:15 -0300

    add variable host

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ var express = require('express');
 const { searchCandidates } = require('./queries');
 
 var app = express();
+var host = 'localhost';
 var port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/candidates', searchCandidates);
+app.get('/candidates', searchCandidates);
 
-app.listen(port, function() {
-    console.log('Server listening on port ' + port);
+app.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`);
 });
