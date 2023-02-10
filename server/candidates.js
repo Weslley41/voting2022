e97f3cf5afe041c524adcae2817188b70852d5a7
weslley41 <41weslley@gmail.com>
2023-02-10 19:06:50 -0300

    create queries to elected/not electeds/all candidates

--- server/candidates.js
+++ server/candidates.js
@@ -3,11 +3,20 @@ const { getData } = require('./queries.js');
 async function searchCandidates(request, response) {
     response.header("Access-Control-Allow-Origin", "http://localhost:8000");
 
-    const name = request.query.name.toUpperCase();
+    let elected = request.query.elected;
+    if (elected) {
+        if (elected !== "all") {
+            let filter_elected = elected === "yes" ? "=" : "!=";
+            elected = `WHERE cand_status ${filter_elected} 1`;
+        }
+    } else {
+        var name = request.query.name.toUpperCase();
+    }
+
     const sql = `
         SELECT cand_nome, cargo_nome, cand_votos, cand_status
         FROM votos_cand_estado
-        WHERE cand_nome LIKE '${name}%'
+        ${elected === 'all' ? '' : elected || `WHERE cand_nome LIKE '${name}%'`}
         ORDER BY cand_votos DESC
     `;
 
